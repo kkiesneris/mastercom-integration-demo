81f1dee9e05a59d6ec667afa2f7dd96ba74c97e2
Kristaps Kiesneris <kristaps.kiesneris@gmail.com>
2025-12-06 00:11:31 +0200

    Initial Spring Boot backend skeleton

---
+++ src/main/java/com/tornis/mastercom/mastercom_api/MastercomApiApplication.java
@@ -0,0 +1,13 @@
+package com.tornis.mastercom.mastercom_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MastercomApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MastercomApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tornis/mastercom/mastercom_api/MastercomApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tornis.mastercom.mastercom_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MastercomApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
